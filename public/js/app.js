09eeeafafe3e4bed8155695d27378e3e422d143b
pawel.w <elendil@konto.pl>
2018-02-23 00:15:36 +0100

    websckets are working, the map hangs then

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,9 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var writeToScreen = function (message) {
+    document.body.innerHTML += message + "<br>";
+}
+
+
+var mymap = L.map('mapid').setView([51.1552819, 16.8978038], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', 
 {
@@ -7,4 +12,15 @@ attribution: 'Trapeze test implementation',
 id: 'mapbox.streets'
 }).addTo(mymap);
 
-L.marker([51.5, -0.09]).addTo(mymap);
+L.marker([51.1552819, 16.8978038]).addTo(mymap);
+
+
+var websockerAddress = "ws://"+window.location.host;
+
+//var websocket = new WebSocket(websockerAddress);
+//websocket.onerror = function(evt) { writeToScreen(" Connection Error"); };
+//websocket.onopen = function(evt) { writeToScreen("Connected!!");};
+//websocket.onmessage = function(evt) { 
+	//map.setView(new L.LatLng(evt.data.longitude, evt.data.latitude), 8);
+//writeToScreen(evt.data);
+//};
--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 var httpPort = 8080;
 
+var long = 51.1552819;
+var lat = 16.8978038;
+var jump = 0.0000001;
+
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
+var expressWs = require('express-ws')(app);
+
+app.ws('/', function(ws, req) { });
 
 app.use(express.static(__dirname + '/public'));
-app.use(morgan('dev'));  
+app.use(morgan('dev'));
 
+setInterval(function () {
+	long = long+jump;
+	lat = lat + jump;
+	var newPosition = { longitude : long , latitude : lat};
+	var message = JSON.stringify(newPosition);
+	console.log(message);
+	expressWs.getWss('/').clients.forEach( function (client) {
+		client.send(message);
+	});
+}, 5000);
 
 
 app.listen(httpPort);
 console.log("App listening on port +" + String(httpPort));
+
